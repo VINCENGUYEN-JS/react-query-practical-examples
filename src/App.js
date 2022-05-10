026359a143e3fe13564f7ac9175cf5802e1ce20b
VinceNguyen <nguyenquangv95@gmail.com>
2022-05-10 14:20:02 +0700

    use-infinite-query

--- src/App.js
+++ src/App.js
@@ -1,19 +1,60 @@
+import * as React from "react";
+import { useInfiniteQuery } from "react-query";
+
+const fetchRepositories = async ({ pageParam = 1 }) => {
+  const response = await fetch(
+    `https://api.github.com/search/repositories?q=topic:reactjs&per_page=30&page=${pageParam}`
+  );
+  return response.json();
+};
 function App() {
+  const { data, hasNextPage, fetchNextPage } = useInfiniteQuery(
+    "repositories",
+    fetchRepositories,
+    {
+      getNextPageParam: (lastPage, allPages) => {
+        const maxPages = lastPage.total_count / 30;
+        const nextPage = allPages.length + 1;
+        return nextPage <= maxPages ? nextPage : undefined;
+      },
+    }
+  );
+
+  React.useEffect(() => {
+    let fetching = false;
+    const onScroll = async (event) => {
+      const { scrollHeight, scrollTop, clientHeight } =
+        event.target.scrollingElement;
+      if (!fetching && scrollHeight - scrollTop <= clientHeight * 1.5) {
+        fetching = true;
+        if (hasNextPage) await fetchNextPage();
+        fetching = false;
+      }
+    };
+    document.addEventListener("scroll", onScroll);
+    return () => {
+      document.removeEventListener("scroll", onScroll);
+    };
+  }, []);
+  console.log({ data });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <h1>Infinite Scroll</h1>
+        <ul>
+          {data.pages.map((page) =>
+            page.items.map((item) => (
+              <li key={item.id}>
+                <p>
+                  <b>{item.name}</b>
+                </p>
+                <p>{item.description}</p>
+              </li>
+            ))
+          )}
+        </ul>
+      </main>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css";
+
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={client}>
+      <React.Suspense fallback={<div>Loading</div>}>
+        <App />
+      </React.Suspense>
+    </QueryClientProvider>
   </React.StrictMode>
 );
